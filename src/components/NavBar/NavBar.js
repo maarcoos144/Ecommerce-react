02d6c7393bb3a4b0792d0130aefacba0a2770bbc
maarcoos144 <maarcoos144@hotmail.com>
2023-08-22 23:10:51 -0300

    Primera pre-entrega

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>TECommerce</h3>
+            <div>
+                <button>Celulares</button>
+                <button>Tablets</button>
+                <button>Notebooks</button>
+                <button>PC's de escritorio</button>
+                <button>Monitores</button>
+                <button>Audio</button>
+                <button>Accesorios</button>
+                <button>Software</button>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
